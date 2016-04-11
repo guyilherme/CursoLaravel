a1ac63150b84287a828dfe9b40fcffe0a220e56d
guyilherme <gmcaaa@gmail.com>
2016-04-11 15:54:05 -0300

    Colocando o o form no blade

--- resources/views/categoria/criar.blade.php
+++ resources/views/categoria/criar.blade.php
@@ -1,7 +1,13 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Guilherme
- * Date: 11/04/2016
- * Time: 14:46
- */
+@extends('app')
+
+@section('content')
+    <div class="row">
+        <div class="large-12 medium-12 small-12 columns">
+            <div class="title">Criação de Categorias</div>
+
+            {!! Form::open() !!}
+            {!! Form::close() !!}
+
+        </div>
+    </div>
+@endsection
