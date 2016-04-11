19b46f9b9f175c9d37a745884b49221d860fa63c
guyilherme <gmcaaa@gmail.com>
2016-04-11 14:54:08 -0300

    Adicionando metodo construtor no controler para injetar o objeto na classe e listar

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -2,13 +2,29 @@
 
 namespace CursoLaravel\Http\Controllers;
 
+use CursoLaravel\Categoria;
 use Illuminate\Http\Request;
 
 use CursoLaravel\Http\Requests;
 
 class CategoriaController extends Controller
 {
+    private $categoriaModel;
+
+    public function __construct(Categoria $categoriaModel){
+
+        $this->categoriaModel = $categoriaModel;
+
+    }
+
     public function  index(){
-        return "Listagem de Categorias";
+
+        $categorias = $this->categoriaModel->all();
+
+        return view('categoria.index', compact('categorias'));
+    }
+
+    public function criar(){
+        return view('categoria.criar');
     }
 }
